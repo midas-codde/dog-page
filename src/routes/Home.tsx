96c29431fc866e76fdfbba4def804f7671f68443
Midas <richardisraelmagalhaes@gmail.com>
2023-07-29 17:36:48 -0300

    Seção Races

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,6 +3,11 @@ import '../css/media.css'
 import '../css/Home.css'
 import imgSec1 from '../assets/imgs/dog-sec1.png'
 import imgSec2 from '../assets/imgs/dog-sec2.png'
+import imgSpitz from '../assets/imgs/spitz.png'
+import imgBulldog from '../assets/imgs/bulldog.png'
+import imgShihTzu from '../assets/imgs/shihtzu.png'
+import imgRottweiler from '../assets/imgs/rottweiler.png'
+import imgPug from '../assets/imgs/pug.png'
 import iconSec1 from '../assets/icons/icon-sec1.png'
 import Logo from '../assets/icons/logo-dog.png'
 
@@ -10,6 +15,7 @@ const Home = () => {
    return (
       <>
          <div className="main">
+            <div className="bg"></div>
             <div className="center-full">
                <div className="circle"></div>
                <div className="sec1-left">
@@ -43,9 +49,49 @@ const Home = () => {
                </div>
                <div className="welcome-right">
                   <div className="border-right"></div>
-                   <h2>Bem vindo a <span>Dog club.</span></h2>
-                   <p>Aqui é um lugar totalmente informativo e de entretenimento sobre cachorros, onde você pode tirar suas dúvidas sobre alguma raça específica ou até mesmo conhecer uma nova. Você tem um doguinho de estimação? Que tal descobrir um pouco mais sobre a raça dele, e se você não tem, adote um!</p>  
+                  <h2>Bem vindo a <span>Dog club.</span></h2>
+                  <p>Aqui é um lugar totalmente informativo e de entretenimento sobre cachorros, onde você pode tirar suas dúvidas sobre alguma raça específica ou até mesmo conhecer uma nova. Você tem um doguinho de estimação? Que tal descobrir um pouco mais sobre a raça dele, e se você não tem, adote um!</p>
                </div>
+            </div>
+         </div>
+         <div className="main">
+            <div className="center-full colunm">
+               <div className="races-title">
+                  <div className="races-line-left"></div>
+                  <h1>Raças</h1>
+                  <div className="races-line-right"></div>
+               </div>
+               <div className="races-text">
+                  <p>Essas são algumas das raças mais famosas do mundo</p>
+               </div>
+               <div className="races-container">
+                  <div className="races-content">
+                     <div className="races-number">1</div>
+                     <img src={imgSpitz} />
+                     <h2>Pomerânia <br />(Spitz Alemão)</h2>
+                  </div>
+                  <div className="races-content">
+                     <div className="races-number">2</div>
+                     <img src={imgBulldog} />
+                     <h2>Bulldog Francês</h2>
+                  </div>
+                  <div className="races-content">
+                     <div className="races-number">3</div>
+                     <img src={imgShihTzu} />
+                     <h2>Shih Tzu</h2>
+                  </div>
+                  <div className="races-content">
+                     <div className="races-number">4</div>
+                     <img src={imgRottweiler} />
+                     <h2>Rottweiler</h2>
+                  </div>
+                  <div className="races-content">
+                     <div className="races-number">5</div>
+                     <img src={imgPug} />
+                     <h2>Pug</h2>
+                  </div>
+               </div>
+               <a className="btn-races" href="#">Ver todas as raças</a>
             </div>
          </div>
       </>
